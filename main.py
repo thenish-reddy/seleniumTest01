73ef099a57cf30d3965d73aa5d685d370e1cab10
thenish-reddy <77598174+thenish-reddy@users.noreply.github.com>
2025-01-20 16:35:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-from flask import Flask, request, jsonify
 from dependencies import get_driver
 from Testcases import login_to_salesforce
+from decouple import config
 
-app = Flask(__name__)
+USERNAME = config('APP_USERNAME')
+PASSWORD = config('APP_PASSWORD')
+SECRET_KEY = "26krycrrp6pbjc55"
 
-@app.route('/run-test', methods=['POST'])
-def run_test():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
-    secret_key = data.get("secret_key")
-
-    if not (username and password and secret_key):
-        return jsonify({"error": "Missing required fields"}), 400
-
+def main():
     driver = get_driver()
     try:
-        login_to_salesforce(driver, username, password, secret_key)
-        return jsonify({"message": "Test executed successfully"}), 200
+        login_to_salesforce(driver, USERNAME, PASSWORD, SECRET_KEY)
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        print(f"Error: {e}")
     finally:
         driver.quit()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    main()
